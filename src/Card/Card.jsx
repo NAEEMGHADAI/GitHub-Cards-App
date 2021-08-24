26fadb0b14bb0d28b924c7370baebe8c224d7b11
NAEEMGHADAI <naeemghadai007.com@gmail.com>
2021-08-24 21:36:35 +1100

    Card design

--- src/Card/Card.jsx
+++ src/Card/Card.jsx
@@ -1,19 +1,30 @@
 import React from "react";
+import "../Card/Css/Card.css";
 
 const Card = (props) => {
+	console.log(props);
 	const profile = props;
 	localStorage.setItem("name", profile.name);
 	localStorage.setItem("company", profile.company);
 	localStorage.setItem("avatar_url", profile.avatar_url);
 	return (
-		<div className="ml-4">
-			<div className="github-profile">
-				<img src={localStorage.getItem("avatar_url")} alt="" />
-				<div className="info">
-					<div className="name">{localStorage.getItem("name")}</div>
-					<div className="company">{localStorage.getItem("company")}</div>
+		<div>
+			<div className="containerCard">
+				<div className="github-profile">
+					<img src={profile.avatar_url} alt="" />
+					<div className="info">
+						<div className="name">{profile.name}</div>
+						<div className="company">{profile.company}</div>
+					</div>
+				</div>
+				<div className="overlay">
+					<div className="text">
+						<h5>{localStorage.getItem("name")}</h5>
+						<button className="btn btn-muted">Show More</button>
+					</div>
 				</div>
 			</div>
+			<br />
 		</div>
 	);
 };
